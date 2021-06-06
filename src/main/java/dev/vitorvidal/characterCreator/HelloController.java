e4a5ad2f07eccc103347d0c0d8334405b58b81ef
Vitor Vidal <vitorvidal.dev@gmail.com>
2021-06-06 09:26:07 -0300

    Added initial security configuration.

---
+++ src/main/java/dev/vitorvidal/characterCreator/HelloController.java
@@ -0,0 +1,15 @@
+package dev.vitorvidal.characterCreator;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloController {
+
+    @GetMapping
+    public void getAll() {
+        System.out.println("Hello");
+    }
+}
---
+++ src/test/java/dev/vitorvidal/characterCreator/CharacterCreatorApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.vitorvidal.characterCreator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CharacterCreatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
