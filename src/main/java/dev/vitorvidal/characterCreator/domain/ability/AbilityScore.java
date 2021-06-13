6447190f1ae9042c814abb733309aa327e3733e3
Vitor Vidal <vitorvidal.dev@gmail.com>
2021-06-12 21:27:54 -0300

    Removed unnecessary code

--- src/main/java/dev/vitorvidal/characterCreator/domain/ability/AbilityScore.java
+++ src/main/java/dev/vitorvidal/characterCreator/domain/ability/AbilityScore.java
@@ -3,10 +3,8 @@ package dev.vitorvidal.characterCreator.domain.ability;
 import javax.persistence.*;
 
 @Entity
-@Table(name = "ability_score")
 public class AbilityScore {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private int strength;
     private int dexterity;
@@ -14,7 +12,7 @@ public class AbilityScore {
     private int intelligence;
     private int wisdom;
     private int charisma;
-//
+
 //    @OneToMany(mappedBy = "ability_score")
 //    private static final List<Character> characters = new ArrayList<>();
 
