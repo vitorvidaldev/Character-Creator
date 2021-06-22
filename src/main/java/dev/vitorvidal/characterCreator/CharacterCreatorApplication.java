793440e4c728973560f493b63a429f4860034cd2
Vitor Vidal <vitorvidal.dev@gmail.com>
2021-06-21 21:29:28 -0300

    Adding swagger configuration.

--- src/main/java/dev/vitorvidal/characterCreator/CharacterCreatorApplication.java
+++ src/main/java/dev/vitorvidal/characterCreator/CharacterCreatorApplication.java
@@ -2,8 +2,12 @@ package dev.vitorvidal.characterCreator;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import springfox.documentation.oas.annotations.EnableOpenApi;
 
 @SpringBootApplication
+@EnableWebSecurity
+@EnableOpenApi
 public class CharacterCreatorApplication {
 
 	public static void main(String[] args) {
