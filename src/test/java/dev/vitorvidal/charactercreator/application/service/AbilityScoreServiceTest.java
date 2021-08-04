be0ceafd642c83a68b01e30d98bca5beb30f2285
Vitor Vidal <vitorvidal.dev@gmail.com>
2021-08-03 22:32:28 -0300

    Stated ability score tests implementation

--- src/test/java/dev/vitorvidal/charactercreator/application/service/AbilityScoreServiceTest.java
+++ src/test/java/dev/vitorvidal/charactercreator/application/service/AbilityScoreServiceTest.java
@@ -1,18 +1,39 @@
 package dev.vitorvidal.charactercreator.application.service;
 
+import dev.vitorvidal.charactercreator.domain.model.AbilityScore;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
 class AbilityScoreServiceTest {
 
+    private static AbilityScore abilityScore;
+
+    @BeforeAll
+    public static void setupMock() {
+        abilityScore = Mockito.mock(AbilityScore.class);
+    }
+
     @Test
-    @Disabled("Not Implemented")
-    void testGetAll() {
+    void testsMocks() {
+        Assertions.assertNotNull(abilityScore);
+    }
+
+    @Test
+    void testGetAllStatusCode() {
+        AbilityScoreService serviceMock = Mockito.mock(AbilityScoreService.class);
+        Mockito.when(serviceMock.getAll()).thenReturn(new ResponseEntity<>(HttpStatus.OK));
+        Assertions.assertEquals(HttpStatus.OK, serviceMock.getAll().getStatusCode());
     }
 
     @Test
     @Disabled("Not Implemented")
     void testSetAbilityScore() {
+
     }
 
     @Test
