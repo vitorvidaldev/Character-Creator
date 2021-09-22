ef0bbd64848d7472ee9c4d65e5b08e41213fc4f2
Vitor Vidal <vitorvidal.dev@gmail.com>
2021-09-21 22:29:44 -0300

    Refactoring dice rolls functionality

--- src/test/java/dev/vitorvidal/charactercreator/util/DiceSimulatorTests.java
+++ src/test/java/dev/vitorvidal/charactercreator/util/DiceSimulatorTests.java
@@ -1,4 +1,4 @@
-package dev.vitorvidal.charactercreator.application.controller;
+package dev.vitorvidal.charactercreator.util;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
@@ -8,13 +8,13 @@ import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
-class DiceRollControllerTests {
-    private final DiceRollController controller = new DiceRollController();
+class DiceSimulatorTests {
+    private final DiceSimulator dice = new DiceSimulator();
 
     @Test
     @DisplayName("Tests if the method returns one of the three possible values for a D3")
     void shouldRollAd3() {
-        int d3 = controller.d3();
+        int d3 = dice.d3();
         List<Integer> results = IntStream.rangeClosed(1, 3).boxed().collect(Collectors.toList());
         Assertions.assertTrue(results.contains(d3));
     }
@@ -22,7 +22,7 @@ class DiceRollControllerTests {
     @Test
     @DisplayName("Tests if the method returns one of the four possible values for a D4")
     void shouldRollAd4() {
-        int d4 = controller.d4();
+        int d4 = dice.d4();
         List<Integer> results = IntStream.rangeClosed(1, 4).boxed().collect(Collectors.toList());
         Assertions.assertTrue(results.contains(d4));
     }
@@ -30,7 +30,7 @@ class DiceRollControllerTests {
     @Test
     @DisplayName("Tests if the method returns one of the six possible values for a D6")
     void shouldRollAd6() {
-        int d6 = controller.d6();
+        int d6 = dice.d6();
         List<Integer> results = IntStream.rangeClosed(1, 6).boxed().collect(Collectors.toList());
         Assertions.assertTrue(results.contains(d6));
     }
@@ -38,7 +38,7 @@ class DiceRollControllerTests {
     @Test
     @DisplayName("Tests if the method returns one of the eight possible values for a D8")
     void shouldRollAd8() {
-        int d8 = controller.d8();
+        int d8 = dice.d8();
         List<Integer> results = IntStream.rangeClosed(1, 8).boxed().collect(Collectors.toList());
         Assertions.assertTrue(results.contains(d8));
     }
@@ -46,7 +46,7 @@ class DiceRollControllerTests {
     @Test
     @DisplayName("Tests if the method returns one of the ten possible values for a D10")
     void shouldRollAd10() {
-        int d10 = controller.d10();
+        int d10 = dice.d10();
         List<Integer> results = IntStream.rangeClosed(1, 10).boxed().collect(Collectors.toList());
         Assertions.assertTrue(results.contains(d10));
     }
@@ -54,7 +54,7 @@ class DiceRollControllerTests {
     @Test
     @DisplayName("Tests if the method returns one of the twelve possible values for a D12")
     void shouldRollAd12() {
-        int d12 = controller.d12();
+        int d12 = dice.d12();
         List<Integer> results = IntStream.rangeClosed(1, 12).boxed().collect(Collectors.toList());
         Assertions.assertTrue(results.contains(d12));
     }
@@ -62,7 +62,7 @@ class DiceRollControllerTests {
     @Test
     @DisplayName("Tests if the method returns one of the twenty possible values for a D20")
     void shouldRollAd20() {
-        int d20 = controller.d20();
+        int d20 = dice.d20();
         List<Integer> results = IntStream.rangeClosed(1, 20).boxed().collect(Collectors.toList());
         Assertions.assertTrue(results.contains(d20));
     }
