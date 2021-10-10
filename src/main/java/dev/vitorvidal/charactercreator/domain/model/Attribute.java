4126fc05d148ebf5f370db7fa86a01e87d950437
Vitor Vidal <vitorvidal.dev@gmail.com>
2021-10-10 11:18:14 -0300

    Added lombok annotations to simplify class

--- src/main/java/dev/vitorvidal/charactercreator/domain/model/Attribute.java
+++ src/main/java/dev/vitorvidal/charactercreator/domain/model/Attribute.java
@@ -1,5 +1,10 @@
 package dev.vitorvidal.charactercreator.domain.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class Attribute {
     private int strength;
     private int dexterity;
@@ -9,39 +14,6 @@ public class Attribute {
     private int charisma;
 
     public Attribute() {}
-
-    public Attribute(int strength, int dexterity, int constitution, int intelligence, int wisdom, int charisma) {
-        this.strength = strength;
-        this.dexterity = dexterity;
-        this.constitution = constitution;
-        this.intelligence = intelligence;
-        this.wisdom = wisdom;
-        this.charisma = charisma;
-    }
-
-    public int getStrength() {
-        return strength;
-    }
-
-    public int getDexterity() {
-        return dexterity;
-    }
-
-    public int getConstitution() {
-        return constitution;
-    }
-
-    public int getIntelligence() {
-        return intelligence;
-    }
-
-    public int getWisdom() {
-        return wisdom;
-    }
-
-    public int getCharisma() {
-        return charisma;
-    }
 
     public void updateStrength(int strength) {
         this.strength += strength;
@@ -65,17 +37,5 @@ public class Attribute {
 
     public void updateCharisma(int charisma) {
         this.charisma += charisma;
-    }
-
-    @Override
-    public String toString() {
-        return "Attribute{" +
-                "strength=" + strength +
-                ", dexterity=" + dexterity +
-                ", constitution=" + constitution +
-                ", intelligence=" + intelligence +
-                ", wisdom=" + wisdom +
-                ", charisma=" + charisma +
-                '}';
     }
 }
