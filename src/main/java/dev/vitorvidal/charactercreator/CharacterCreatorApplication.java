da7d1a68e96661c002116974f88773c5e5b20b6f
Vitor Vidal <vitorvidal.dev@gmail.com>
2021-12-21 19:14:16 -0300

    Updating dependencies

--- src/main/java/dev/vitorvidal/charactercreator/CharacterCreatorApplication.java
+++ src/main/java/dev/vitorvidal/charactercreator/CharacterCreatorApplication.java
@@ -3,11 +3,9 @@ package dev.vitorvidal.charactercreator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import springfox.documentation.oas.annotations.EnableOpenApi;
 
 @SpringBootApplication
 @EnableWebSecurity
-@EnableOpenApi
 public class CharacterCreatorApplication {
 
     public static void main(String[] args) {
