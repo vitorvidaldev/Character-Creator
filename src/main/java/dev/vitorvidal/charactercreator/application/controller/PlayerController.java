170136d958d324d3526026c93bf8bb5b87c39bca
Vitor Vidal <vitorvidal.dev@gmail.com>
2022-04-13 21:03:42 -0300

    Updating endpoint paths

--- src/main/java/dev/vitorvidal/charactercreator/application/controller/PlayerController.java
+++ src/main/java/dev/vitorvidal/charactercreator/application/controller/PlayerController.java
@@ -10,7 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/rest/v1/player")
+@RequestMapping("/rest/v1/players")
 public class PlayerController {
     private final PlayerService playerService;
 
--- src/main/java/dev/vitorvidal/charactercreator/application/controller/UserController.java
+++ src/main/java/dev/vitorvidal/charactercreator/application/controller/UserController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 
 @RestController
-@RequestMapping("/rest/v1/user")
+@RequestMapping("/rest/v1/users")
 public class UserController {
 
     private final UserService userService;
